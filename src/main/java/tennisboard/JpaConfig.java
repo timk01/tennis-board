195f2a0ae3bf5b2f7de2a14d01d29b15b058894d
Timur <timk01@mail.ru>
2026-07-26 22:37:05 +0300

    tennis-board: - рефактор JpaConfig;

--- src/main/java/tennisboard/JpaConfig.java
+++ src/main/java/tennisboard/JpaConfig.java
@@ -19,6 +19,18 @@ import java.util.Properties;
 @EnableTransactionManagement
 @PropertySource(value = {"classpath:database.properties"})
 public class JpaConfig {
+    private static final String HIBERNATE_DIALECT_KEY = "hibernate.dialect";
+    private static final String HIBERNATE_HBM_2_DDL_AUTO_KEY = "hibernate.hbm2ddl.auto";
+    private static final String HIBERNATE_SHOW_SQL_KEY = "hibernate.show_sql";
+    private static final String HIBERNATE_FORMAT_SQL_KEY = "hibernate.format_sql";
+
+
+    private static final String PACKAGE_TO_SCAN = "tennisboard.entity";
+
+
+    @Value("${db.driver.name}")
+    private String driverName;
+
     @Value("${db.url}")
     private String dbUrl;
 
@@ -31,10 +43,22 @@ public class JpaConfig {
     @Value("${db.pool.size}")
     private int poolSize;
 
+    @Value("${hibernate.dialect}")
+    private String dialect;
+
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String hbm2ddlAuto;
+
+    @Value("${hibernate.show_sql}")
+    private String showSql;
+
+    @Value("${hibernate.format_sql}")
+    private String formatSql;
+
     @Bean
     public DataSource getDataSource() {
         HikariConfig config = new HikariConfig();
-        config.setDriverClassName("org.postgresql.Driver");
+        config.setDriverClassName(driverName);
         config.setJdbcUrl(dbUrl);
         config.setPassword(dbPassword);
         config.setUsername(dbUsername);
@@ -48,16 +72,9 @@ public class JpaConfig {
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
         entityManagerFactoryBean.setDataSource(dataSource);
         entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        entityManagerFactoryBean.setPackagesToScan("tennisboard.entity");
+        entityManagerFactoryBean.setPackagesToScan(PACKAGE_TO_SCAN);
 
-        Properties jpaProperties = new Properties();
-
-        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        jpaProperties.put("hibernate.hbm2ddl.auto", "none");
-        jpaProperties.put("hibernate.show_sql", "true");
-        jpaProperties.put("hibernate.format_sql", "true");
-
-        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+        entityManagerFactoryBean.setJpaProperties(createJpaProperties());
 
         return entityManagerFactoryBean;
     }
@@ -68,5 +85,16 @@ public class JpaConfig {
         transactionManager.setEntityManagerFactory(entityManagerFactory);
 
         return transactionManager;
+    }
+
+    private Properties createJpaProperties() {
+        Properties jpaProperties = new Properties();
+
+        jpaProperties.put(HIBERNATE_DIALECT_KEY, dialect);
+        jpaProperties.put(HIBERNATE_HBM_2_DDL_AUTO_KEY, hbm2ddlAuto);
+        jpaProperties.put(HIBERNATE_SHOW_SQL_KEY, showSql);
+        jpaProperties.put(HIBERNATE_FORMAT_SQL_KEY, formatSql);
+
+        return jpaProperties;
     }
 }
